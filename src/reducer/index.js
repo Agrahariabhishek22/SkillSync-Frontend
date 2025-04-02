92a31d0fef755e8bfd7a5ad4acceed735c809143
Abhishek Agrahari <132470521+Agrahariabhishek22@users.noreply.github.com>
2025-04-02 23:28:19 +0530

    Update index.js

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import profileReducer from "../slices/profileSlice"
 import cartReducer from "../slices/cartSlice";
-import authReducer from "../slices/authSlice";
+import authReducer from "../slices/AuthSlice";
 import viewCourseReducer from "../slices/viewCourseSlice";
 import courseReducer from"../slices/courseSlice";
 
